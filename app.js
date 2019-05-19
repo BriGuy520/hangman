c8d0b72a9aa6e6419acd9e03708fce1245b61597
Brian Falasz <bfalasz@yahoo.com>
2019-05-18 23:58:51 -0500

    added a css file. Got individual p to display inline

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const displayQuote = document.querySelector('.quote');
 const url = `http://quotes.rest/qod.json?category=management`;
 
 
+// this will append each letter from the quote to the dom
 function createNode(element){
-    let newNode = document.createElement('div')
+    let newNode = document.createElement('p')
     let htmlElement = document.body.appendChild(newNode);
     htmlElement.innerHTML = element;
-
 }
 
+
+// this will be our promise to get our quote from the API
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
         const quote = data.contents.quotes[0].quote;
 
         quote.split('').forEach(char => {
+            // call the createNode function from line 6
             return createNode(char);
         });
     })
