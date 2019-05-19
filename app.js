5613c5a4a4ffb712dc777bd4b30ac55eea5a10ed
Brian Falasz <bfalasz@yahoo.com>
2019-05-19 01:34:51 -0500

    set up the hangman stage

--- app.js
+++ app.js
@@ -1,6 +1,45 @@
 const displayQuote = document.querySelector('.quote');
 const displayhAuthor = document.querySelector('.author');
+
 const url = `http://quotes.rest/qod.json?category=management`;
+
+// setting up the hangman canvas
+function draw(){
+    const canvas = document.getElementById('hangman');
+    const ctx = canvas.getContext('2d');
+
+    const podium = 'rgb(204, 102, 0)';
+    const rope = 'white';
+
+    //base
+    ctx.beginPath();
+    ctx.strokeStyle = podium;
+    ctx.moveTo(100, 120)
+    ctx.lineTo(10, 120);
+    ctx.stroke();
+
+    // pole
+    ctx.beginPath();
+    ctx.stokeStyle = podium;
+    ctx.moveTo(60, 20);
+    ctx.lineTo(60, 120);
+    ctx.stroke();
+
+    // bar
+    ctx.beginPath();
+    ctx.stokeStyle = podium;
+    ctx.moveTo(140, 20);
+    ctx.lineTo(60, 20);
+    ctx.stroke(); 
+
+    // rope
+    ctx.beginPath();
+    ctx.stokeStyle = podium;
+    ctx.moveTo(140, 20);
+    ctx.lineTo(140, 40);
+    ctx.stroke();
+
+}
 
 // this will append each letter from the quote to the dom
 function createNode(element){
