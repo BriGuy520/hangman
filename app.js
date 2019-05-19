31d8177190808a009cdf26a736ca325c1c0f947d
Brian Falasz <bfalasz@yahoo.com>
2019-05-18 23:47:51 -0500

    able to get the letters to append to the dom from the quote api

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const displayQuote = document.querySelector('.quote');
 const url = `http://quotes.rest/qod.json?category=management`;
 
 
+function createNode(element){
+    let newNode = document.createElement('div')
+    let htmlElement = document.body.appendChild(newNode);
+    htmlElement.innerHTML = element;
+
+}
 
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
         const quote = data.contents.quotes[0].quote;
+
+        quote.split('').forEach(char => {
+            return createNode(char);
+        });
     })
     .catch((err) => {
         console.log(err);
