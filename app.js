6f0afcd4ad132e813b572500755ff155d3be4f95
Brian Falasz <bfalasz@yahoo.com>
2019-05-19 00:40:47 -0500

    added author to the dom as well as added an input and submit button

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const displayQuote = document.querySelector('.quote');
+const displayhAuthor = document.querySelector('.author');
 const url = `http://quotes.rest/qod.json?category=management`;
-
 
 // this will append each letter from the quote to the dom
 function createNode(element){
@@ -9,17 +9,29 @@ function createNode(element){
     htmlElement.innerHTML = element;
 }
 
+function showAuthor(name){
+    let authorNode = document.createElement('p');
+    let appendAuthor = document.body.appendChild(authorNode);
+    appendAuthor.setAttribute('class', 'author')
+    appendAuthor.innerHTML = "- " + name;
+}
+
 
 // this will be our promise to get our quote from the API
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
         const quote = data.contents.quotes[0].quote;
+        const author = data.contents.quotes[0].author;
 
         quote.split('').forEach(char => {
             // call the createNode function from line 6
             return createNode(char);
         });
+
+        return showAuthor(author);
+
+        
     })
     .catch((err) => {
         console.log(err);
